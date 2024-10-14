201bd0bbf31188b90a356cf119ca8e29bc8758d4
Derek Hansen <derek.l.hansen-2@ou.edu>
2024-10-14 16:26:40 -0500

    added Data for lab 7

---
+++ R/data-fire.R
@@ -0,0 +1,11 @@
+#' Fire Damage Data
+#'
+#' A dataset containing information on fire damage for analysis purposes.
+#'
+#' @format A data frame with 100 rows and the following columns:
+#' \describe{
+#'   \item{Distance}{Distance in miles}
+#'   \item{Damage}{Damage in million of dollars}
+#' }
+#' @source MATH 4753 resources
+"fire"
